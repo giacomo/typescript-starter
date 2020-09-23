0f85f95f0a9135979a3a841daca29c787e6021aa
Giacomo Barbalinardo <info@ready24it.eu>
2020-09-23 08:42:11 +0200

    fix(code-style): space of curly braces

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // enable es native es5 custome-elements
 import '@webcomponents/custom-elements/src/native-shim';
 
-import { Tooltip } from './components/tooltip/Tooltip';
-import { Framework } from './lib/Framework';
+import {Tooltip} from './components/tooltip/Tooltip';
+import {Framework} from './lib/Framework';
 
 // write your code here.
 const app = new Framework();
